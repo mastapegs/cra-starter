985e5f5b40e8ced8399679832cb805969e0f8cbd
Matthew Pagan <mastapegs01@gmail.com>
2021-09-03 21:35:07 -0400

    add husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { FC } from "react";
+
+const App: FC = () => {
   return (
     <div className="bg-gray-50 p-4 min-h-screen">
       <h1>CRA Starter</h1>
     </div>
   );
-}
+};
 
 export default App;
